217bdcc0feb98ab5670a8f677140860787136edd
Terje Topland <terje.topland@forsta.com>
2023-08-29 09:00:55 +0200

    working with css

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
-import './App.css';
+// import './App.css';
+import './index.css'
 import React from 'react';
 import JsonReader from './components/JsonReader';
 
 
 function App() {
   return (
-    <div>
-      blarg
-      <JsonReader/>
+    <div className='container'>
+      <h1 >H1 element</h1>
+      <p>This a pargraph with styling</p>
+      <div className="myPara">This is a class in css</div>
+      {/* <JsonReader/> */}
       </div>
     
 
