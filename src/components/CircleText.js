bf8e3bd3ab52a43514822a7225b60caa4bcb064c
terjetop <terjetop@uia.no>
2023-09-01 15:19:10 +0200

    still trying to figure out how to rotate a hole word based on angel

--- src/components/CircleText.js
+++ src/components/CircleText.js
@@ -1,14 +1,30 @@
 export const CircleText = () => {
-
+    
+    function rotatText(someText) {
+        let newText = [];
+        let rotateAngel = [];
+        for(let i = 0; i < someText.length; i++) {
+            newText += someText[i]
+            rotateAngel[i] += 10;
+        }
+        return (
+            <div>{newText}</div>
+        )
+    }
     return (
         <div className="container">
             <div className="circle-one">
                 <div className="circle-two">
-                    <div className="circle-two" style={{backgroundColor: 'red'}}></div>
+                    <div className="circle-two" style={{backgroundColor: 'red'}}>
+                        
+                    </div>
                 </div>
             </div>
             <div className="circle-one">
-                <div className="circle-two"></div>
+                <div className="circle-two">
+                    <div className="rotate-text">{rotatText('test')}</div>
+                </div>
+                
             </div>
         </div>        
        
